b5777ef275ce5e22c5019a93be770bbdf415fbb6
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2023-02-06 16:36:21 +0000

    fix space issues cont

--- config.ts
+++ config.ts
@@ -1,13 +1,15 @@
-import * as dotenv from 'dotenv'
-import { ConfigModel } from '@models'
+import * as dotenv from "dotenv"
+import { ConfigModel } from "@models"
 
 dotenv.config()
 
 const config: ConfigModel = {
-    SERVER_URL: new URL(process.env.SERVER_URL as string),
-    REFRESH_RATE_MILLISECONDS: Number(process.env.REFRESH_RATE_MILLISECONDS as string),
-    SCREEN_ROWS: 30,
-    COLUMN_WIDTH: 20
-}
+  SERVER_URL: new URL(process.env.SERVER_URL as string),
+  REFRESH_RATE_MILLISECONDS: Number(
+    process.env.REFRESH_RATE_MILLISECONDS as string
+  ),
+  SCREEN_ROWS: 30,
+  COLUMN_WIDTH: 20,
+};
 
 export default config
