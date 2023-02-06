913373f5733b7561ef0921cf4eef7e1c49676029
Abdullah Chaudhry <19255312+AbdullahChaudhry@users.noreply.github.com>
2023-02-06 16:38:15 +0000

    more fixes

--- config.ts
+++ config.ts
@@ -5,11 +5,9 @@ dotenv.config()
 
 const config: ConfigModel = {
   SERVER_URL: new URL(process.env.SERVER_URL as string),
-  REFRESH_RATE_MILLISECONDS: Number(
-    process.env.REFRESH_RATE_MILLISECONDS as string
-  ),
+  REFRESH_RATE_MILLISECONDS: Number(process.env.REFRESH_RATE_MILLISECONDS as string),
   SCREEN_ROWS: 30,
   COLUMN_WIDTH: 20,
-};
+}
 
 export default config
